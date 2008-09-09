8683ab3952d34f8f6be842f1e24bd546689c0105
John Goerzen <jgoerzen@complete.org>
2008-09-09 16:12:05 -0500

    Initial add of 2flickr.py

---
+++ 2flickr.py
@@ -0,0 +1,15 @@
+import simplejson
+import flickrapi
+
+import sys, os, urllib
+
+flickrapikey = sys.argv[1]
+flickrapisecret = sys.argv[2]
+scandir = sys.argv[3]
+
+flickr = flickrapi.FlickrAPI(flickrapikey, flickrapisecret)
+
+(token, frob) = flickr.get_token_part_one(perms='write')
+if not token: raw_input("Press ENTER after you authorized this program")
+flickr.get_token_part_two((token, frob))
+
